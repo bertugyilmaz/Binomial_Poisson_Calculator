7b7d5c452cb3009b0b56d0eb489f4c20c122d85e
Bertuğ Yılmaz <ylmazbertug@gmail.com>
2018-05-10 17:28:23 +0300

    init project

---
+++ binomial_poisson_calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  binomial_poisson_calculator
+//
+//  Created by Bertuğ Yılmaz on 10.05.2018.
+//  Copyright © 2018 Bertuğ Yılmaz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
